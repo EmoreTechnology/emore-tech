e0e25e99b31840920f5d8d0306f3f0f1abbf17d7
emore23 <iurymarmore@gmail.com>
2022-04-28 20:51:00 -0300

    feat(front): adicionado husky e lint staged

--- src/components/about-us/about-us.styles.ts
+++ src/components/about-us/about-us.styles.ts
@@ -4,7 +4,6 @@ export const Container = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: center;
-
     align-items: center;
     background: rgba(44, 69, 141, 0.3);
     height: 800px;
