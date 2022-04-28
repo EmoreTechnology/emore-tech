7e051b02f19237ba61960292485b03ff03f773fb
emore23 <iurymarmore@gmail.com>
2022-04-28 20:25:09 -0300

    feat(front): adicionado husky e lint staged

--- src/page/home/home.page.tsx
+++ src/page/home/home.page.tsx
@@ -23,27 +23,19 @@ const Home: React.FC = () => {
 
       <div className="home">
         <Particles
-
-
-
           id="tsparticles"
           className="tsparticles"
+
+
+					
           options={particlesOptions as ISourceOptions}
           init={particlesInit}
         />
       </div>
 
-
-			
-
       <div className="aboutUs">
         <AboutUs />
       </div>
-
-
-
-
-
     </Container>
   );
 };
