1496abebbbb7b2eeae0b0a67a64cf8ef627ede0b
emore23 <iurymarmore@gmail.com>
2022-04-28 20:53:52 -0300

    feat(front): adicionado husky e lint staged

--- src/components/about-us/about-us.component.tsx
+++ src/components/about-us/about-us.component.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import Card from "../card/card.component";
-import { Container } from "./about-us.styles";
+import React from 'react';
+import Card from '../card/card.component';
+import { Container } from './about-us.styles';
 
 const AboutUs: React.FC = () => {
-  return (
-    <Container>
-      <Card />
-    </Container>
-  );
+    return (
+        <Container>
+            <Card />
+        </Container>
+    );
 };
 
 export default AboutUs;
