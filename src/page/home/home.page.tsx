dcf6e3e14e8a453c532f3e60f37df879068b48b9
emore23 <iurymarmore@gmail.com>
2022-04-28 20:47:28 -0300

    feat(front): adicionado husky e lint staged

--- src/page/home/home.page.tsx
+++ src/page/home/home.page.tsx
@@ -1,46 +1,40 @@
 // Dependencies
-import React, { useCallback } from "react";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
-import type { Engine } from "tsparticles-engine";
-import { ISourceOptions } from "tsparticles-engine";
-import AboutUs from "../../components/about-us/about-us.component";
-import Header from "../../components/header/header.component";
+import React, { useCallback } from 'react';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+import type { Engine } from 'tsparticles-engine';
+import { ISourceOptions } from 'tsparticles-engine';
 // Components
+import AboutUs from '../../components/about-us/about-us.component';
+import Header from '../../components/header/header.component';
 // Utils
-import particlesOptions from "../../particles.json";
+import particlesOptions from '../../particles.json';
 // Styles
-import { Container } from "./home.styles";
+import { Container } from './home.styles';
 
 const Home: React.FC = () => {
-  const particlesInit = useCallback((engine: Engine): any => {
-    loadFull(engine);
-  }, []);
+    const particlesInit = useCallback((engine: Engine): any => {
+        loadFull(engine);
+    }, []);
 
-  return (
-    <Container>
-      <Header />
+    return (
+        <Container>
+            <Header />
 
-      <div className="home">
-        <Particles
-          id="tsparticles"
+            <div className="home">
+                <Particles
+                    id="tsparticles"
+                    className="tsparticles"
+                    options={particlesOptions as ISourceOptions}
+                    init={particlesInit}
+                />
+            </div>
 
-
-					
-          className="tsparticles"
-
-
-
-          options={particlesOptions as ISourceOptions}
-          init={particlesInit}
-        />
-      </div>
-
-      <div className="aboutUs">
-        <AboutUs />
-      </div>
-    </Container>
-  );
+            <div className="aboutUs">
+                <AboutUs />
+            </div>
+        </Container>
+    );
 };
 
 export default Home;
