1958f730aa43a54bf9b1719eb55cc5bf2d42468e
emore23 <iurymarmore@gmail.com>
2022-04-28 21:45:22 -0300

    fix(ajuste index): ajuste

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Home from "./page/home/home.page";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from './page/home/home.page';
 
 ReactDOM.render(
   <React.StrictMode>
     <Home />
+    <p>teste</p>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
